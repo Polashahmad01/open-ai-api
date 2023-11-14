2fb0946381f4de1f7ddff83190d250899d3b53b8
Polashahmad01 <dev.polashahmad@gmail.com>
2023-11-14 23:19:41 +0600

    Add initial project files and dependencies

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const morgan = require("morgan")
+const colors = require("colors")
+const cors = require("cors")
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+if(process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"))
+}
+
+app.use(cors())
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, message: "Welcome to Open AI APIS" })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+})
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // Close app & exit process
+  app.close(() => process.exit(1))
+})
